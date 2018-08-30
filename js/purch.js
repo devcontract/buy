c7652f55b83a7172b094b4767abd2ab8d5c8a108
devcontract <38960225+devcontract@users.noreply.github.com>
2018-08-31 00:39:48 +0300

    2

--- js/purch.js
+++ js/purch.js
@@ -1,4 +1,20 @@
+
+
+
+
 $(document).ready(function() {
+var countdown;
+  $(function(){
+  //  FlipClock.Lang.Custom = { days:'', hours:'', minutes:'', seconds:'' };
+    var opts = {
+      clockFace: 'DailyCounter',
+      countdown: true,
+      language: 'Custom'
+    };
+     countdown = 1535742540 - ((new Date().getTime())/1000); // from: 08/31/2018 10:09 pm +0300
+    countdown = Math.max(1, countdown);
+    $('.clock-builder-output').FlipClock(countdown, opts);
+  });
 
   $("#ether-button").click(function(){
         $("#multiCollapseExample1").collapse('hide');
@@ -16,6 +32,10 @@ $(document).ready(function() {
         $("#multiCollapseExample1").collapse('show');
         $("#currency").text("EUR");
     });
+
+$("#timeText").text("Sale opens in:");
+
+
 
 
 });
