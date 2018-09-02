2ef1baed0266764205b9d8c2d67912670b0e90a7
kosankuz <konstantinkuzmenko@icloud.com>
2018-09-02 21:28:54 +0100

    IDs

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+
+/*Investment block*/
+  document.getElementById('ether-button');
+  document.getElementById('bitcoin-button');
+  $(".currencyName").text("ETH");
+  $(".tokenName").text("TES");
+  $("#tokenPerEth").text("1000");
+  $("#stage-1pct").text("50%");
+  $("#stage-2pct").text("30%");
+  $("#stage-3pct").text("20%");
+  $("#stage-4pct").text("0%");
+
+  $("#bonus-1pct").text("50%");
+  $("#bonus-2pct").text("30%");
+  $("#bonus-3pct").text("20%");
+  $("#bonus-4pct").text("0%");
+
+  $("#ether-1E").text("E1");
+  $("#ether-2E").text("E5");
+  $("#ether-3E").text("E5");
+  $("#ether-4E").text("E10");
+
+  $("#standartPrice").text("10");
+  $("#calcbonus").text("22222");
+  $("#tokenAmount").text("100");
+
+  $("#minimumInvestment").text("500");
+  $("#paymentAddressInput");
+
+  $("#gasLimitValue").text("21000");
+  $("#gasPriceValue").text("9");
+
+
+
+});
