b3efe0d0373c15b80b103609056a2a5ad2541d63
kosankuz <konstantinkuzmenko@icloud.com>
2018-09-09 21:49:37 +0100

    .

--- ico/purchase_token/js/index.js
+++ ico/purchase_token/js/index.js
@@ -1,13 +1,12 @@
+import {minInput} from './mininput.js'
+
 $(document).ready(function(){
+minInput();
 
 $("#ether-button").trigger('click');
-
-
-  document.getElementById('ether-button');
-  document.getElementById('bitcoin-button');
-  $("#saleStatus").text("going to finish in");
-
-
+document.getElementById('ether-button');
+document.getElementById('bitcoin-button');
+$("#saleStatus").text("going to finish in");
   $(".cryptoCurrencyName").text("ETH");
   $(".tokenName").text("TES");
   $("#tokenPerEth").text("1000");
@@ -19,42 +18,32 @@ $("#ether-button").trigger('click');
 var bonusBarVisible = true;
 var stageBar = true;
 
-
   $("#bonus-1pct").text("10%");
   $("#bonus-2pct").text("35%");
   $("#bonus-3pct").text("75%");
   $("#bonus-4pct").text("100%");
-
   $("#ether-1E").text("E5");
   $("#ether-2E").text("E10");
   $("#ether-3E").text("E20");
   $("#ether-4E").text("E40");
-
   $("#standartPrice").text("100000000");
   $("#calcbonus").text("22220000");
   $("#tokenAmount").text("200000000000");
   $("#saleBonus1").text("1232");
   $("#saleBonus2").text("8888000");
-
-
   $("#minimumInvestment").text("500");
   $("#paymentAddressInput");
-
   $("#gasLimitValue").text("21000");
   $("#gasPriceValue").text("9");
   $("#paymentCurrency").text("Ethereum");
 
-
-
   $("#bitcoin-button").click(function() {
-    $("#etherButtonsHide").hide();
-    $("#paymentCurrency").text("Bitcoin");
+  $("#etherButtonsHide").hide();
+  $("#paymentCurrency").text("Bitcoin");
   });
+
   $("#ether-button").click(function() {
-    $("#etherButtonsHide").show();
-    $("#paymentCurrency").text("Ethereum");
+  $("#etherButtonsHide").show();
+  $("#paymentCurrency").text("Ethereum");
   });
-
-
-
 });
--- ico/purchase_token/js/mininput.js
+++ ico/purchase_token/js/mininput.js
@@ -1,18 +1,10 @@
 
-  setInterval(function() {                //this function calculates % and bonus
+export function minInput(){  setInterval(function() {                //this function calculates % and bonus
     var x = (0.3 * 1 + 0.2 * 1) / 100;
-
-    if ($("#inputAmount").val() <= x) {
-
+  if ($("#inputAmount").val() <= x) {
       $("#inputAmount").css("border", "2px solid red");
-
       //token_calc.innerHTML = "Min Limit";
-
     } else {
-
-
 $("#inputAmount").css("border", "none");
-
-
-    }
-  }, 10)
+}
+  }, 10)}
