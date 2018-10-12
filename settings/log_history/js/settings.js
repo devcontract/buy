4b18088c493f4445374cf3bc1b65b84096fb23f9
kosankuz <konstantinkuzmenko@icloud.com>
2018-10-12 20:51:52 +0100

    .

---
+++ settings/log_history/js/settings.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  if (document.cookie == "currency=USD"){
+    $("#usd").attr('checked',true);
+  } else {
+    $("#eur").attr('checked',true);
+  }
+
+setInterval(function(){
+  if ($("#usd").is(':checked')) {
+  document.cookie = "currency=USD; path=/;";
+    } else if ($("#eur").is(':checked')){
+  document.cookie = "currency=EUR; path=/;";
+  }
+},100);
+
+
+
+$("#showme").click(function(){
+var cookie = document.cookie;
+alert(cookie);
+})
+
+
+
+});
