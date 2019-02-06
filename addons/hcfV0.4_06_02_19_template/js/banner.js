d152b4539abba222f8a6b63fb1770cfa01792a38
kosankuz <konstantinkuzmenko@icloud.com>
2019-02-06 22:18:24 +0000

    .

---
+++ addons/hcfV0.4_06_02_19_template/js/banner.js
@@ -0,0 +1,12 @@
+//$(document).ready(function() {
+//      $(window).scroll(function(event) {
+//          let scroll = $(this).scrollTop();
+//          let opacity = 0.200 + (scroll / 1000);
+//  $('#opacity').text(opacity);
+//          if (opacity > 0) {
+//            
+//              $('#banner').css('opacity', opacity);
+//
+//          }
+//      });
+//  });
