047af2335cf570c07369b1a610e7a68069659fea
kosankuz <konstantinkuzmenko@icloud.com>
2019-02-08 23:31:53 +0000

    .

--- addons/hcfV0.4_06_02_19/js/banner.js
+++ addons/hcfV0.4_06_02_19/js/banner.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
       $(window).scroll(function(event) {
           let scroll = $(this).scrollTop();
-          let opacity = 0.200 + (scroll / 1000);
+          let opacity = 0.350 + (scroll / 1000);
   $('#opacity').text(opacity);
           if (opacity > 0) {
-            
+
               $('#banner').css('opacity', opacity);
 
           }
