cb5d5feb332a561a4f56de899291e32a4b636d1c
kosankuz <konstantinkuzmenko@icloud.com>
2019-02-18 22:51:34 +0000

    .

--- addons/captcha_js/js/cap.js
+++ addons/captcha_js/js/cap.js
@@ -1,2 +1,16 @@
 
- alert("hello");
+
+    function submitUserForm(){
+      var response = grecaptcha.getResponse();
+      console.log(response.length);
+      if(response.length == 0){
+        document.getElementById('g-recaptcha-error').innerHTML = '<span style="color:red" > this field is required </span>';
+        return false;
+      }
+      return true;
+    }
+
+   function verifyCaptcha(){
+     console.log('verified');
+     document.getElementById('g-recaptcha-error').innerHTML = '';
+   }
