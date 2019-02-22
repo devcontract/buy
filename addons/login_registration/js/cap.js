8a330614d325bc158fdb22d89addd7df59bf3745
kosankuz <konstantinkuzmenko@icloud.com>
2019-02-22 22:51:38 +0000

    .

--- addons/login_registration/js/cap.js
+++ addons/login_registration/js/cap.js
@@ -1,11 +1,11 @@
 
     var CaptchaCallback = function() {
-        grecaptcha.render('RecaptchaField1', {'sitekey' : '6LfFBZIUAAAAAK6tBMFZL51aWitAL1pYFInu0lip'});
-        grecaptcha.render('RecaptchaField2', {'sitekey' : '6LfFBZIUAAAAAK6tBMFZL51aWitAL1pYFInu0lip'});
+        grecaptcha.render('RecaptchaField', {'sitekey' : '6LfFBZIUAAAAAK6tBMFZL51aWitAL1pYFInu0lip'});
+
     };
 
 
-    function submitLoginForm(){
+    function submitForm(){
       var response = grecaptcha.getResponse();
       console.log(response.length);
       if(response.length == 0){
@@ -18,19 +18,4 @@
     function verifyCaptcha(){
      console.log('verified');
      document.getElementById('g-recaptcha-error').innerHTML = '';
-    }
-
-    function submitRegForm(){
-      var response = grecaptcha.getResponse();
-      console.log(response.length);
-      if(response.length == 0){
-        document.getElementById('g-recaptcha-error0').innerHTML = '<span style="color:red" > this field is required </span>';
-        return false;
-      }
-      return true;
-    }
-
-    function verifyCaptcha(){
-     console.log('verified');
-     document.getElementById('g-recaptcha-error0').innerHTML = '';
     }
